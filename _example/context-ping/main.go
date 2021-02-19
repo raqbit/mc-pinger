15fdafe2454a9ac8cf3b0eb529ad80c78cabbd1e
Herobone <theherobone@gmail.com>
2021-02-19 10:34:45 +0100

    Add example for how to use a ping with context

---
+++ _example/context-ping/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	mcpinger "github.com/Raqbit/mc-pinger"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sig
+		// NOW cancel
+		fmt.Println("aborting due to interrupt...")
+		cancel()
+	}()
+
+	// Create new Pinger with 10 seconds Timeout
+	pinger := mcpinger.NewContext(ctx, "mc.herobone.de", 25565)
+	// Get server info
+	info, err := pinger.Ping()
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Print server info
+	fmt.Printf("Description: \"%s\"\n", info.Description.Text)
+	fmt.Printf("Online: %d/%d\n", info.Players.Online, info.Players.Max)
+	fmt.Printf("Version: %s\n", info.Version.Name)
+}
