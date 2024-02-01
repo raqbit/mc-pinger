082e865633ed17ae85c4994bc9cdce44fd0931ab
Geoff Bourne <itzgeoff@gmail.com>
2024-02-01 15:50:13 -0600

    Handle string list for extra siblings of description

--- chat.go
+++ chat.go
@@ -6,15 +6,16 @@ import (
 
 // RegularChatComponent is a Minecraft chat component
 // See: https://wiki.vg/Chat#Current_system_.28JSON_Chat.29
+// and https://wiki.vg/Text_formatting#Content_fields
 type RegularChatComponent struct {
-	Text          string                 `json:"text"`          // Text content
-	Bold          bool                   `json:"bold"`          // Component is emboldened
-	Italic        bool                   `json:"italic"`        // Component is italicized
-	Underlined    bool                   `json:"underlined"`    // Component is underlined
-	Strikethrough bool                   `json:"strikethrough"` // Component is struck out
-	Obfuscated    bool                   `json:"obfuscated"`    // Component randomly switches between characters of the same width
-	Color         string                 `json:"color"`         // Contains the color for the component
-	Extra         []RegularChatComponent `json:"extra"`         // RegularChatComponent siblings
+	Text          string          `json:"text"`          // Text content
+	Bold          bool            `json:"bold"`          // Component is emboldened
+	Italic        bool            `json:"italic"`        // Component is italicized
+	Underlined    bool            `json:"underlined"`    // Component is underlined
+	Strikethrough bool            `json:"strikethrough"` // Component is struck out
+	Obfuscated    bool            `json:"obfuscated"`    // Component randomly switches between characters of the same width
+	Color         string          `json:"color"`         // Contains the color for the component
+	Extra         []ChatComponent `json:"extra"`         // siblings
 }
 
 // ChatComponent wraps a RegularChatComponent for parsing both regular & string-only MOTD's
@@ -25,6 +26,10 @@ type ChatComponent struct {
 // UnmarshalJSON unmarshals the JSON data
 func (c *ChatComponent) UnmarshalJSON(data []byte) error {
 	var regular RegularChatComponent
+
+	// data can be
+	// {"text":"Foo"}
+	// "Bar"
 
 	// The data starts with quotes which means it's a string, not an object
 	if data[0] == '"' {
