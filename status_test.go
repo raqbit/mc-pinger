6ce5b6a0f36f3da0af8e3a28808df518a43e5d56
Geoff Bourne <itzgeoff@gmail.com>
2024-02-05 09:47:15 -0600

    Add test case and switched to table driven test

--- status_test.go
+++ status_test.go
@@ -1,77 +1,112 @@
 package mcpinger
 
 import (
-	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 )
 
 const (
-	TestDataDir       = "testdata" // Name of testdata directory
-	VersionName       = "1.13.2"
-	Protocol          = 404
-	Description       = "Hello world"
-	MaxPlayers        = 100
-	OnlinePlayers     = 5
-	PlayerSampleCount = 1
-	PlayerSampleName  = "Raqbit"
-	PlayerSampleUuid  = "09bc745b-3679-4152-b96b-3f9c59c42059"
-	Favicon           = "data:image/png;base64,<data>"
+	TestDataDir = "testdata" // Name of testdata directory
 )
 
 func TestParseServerInfo(t *testing.T) {
-	files := []string{
-		"info.json",
-		"info_description_string.json",
+	tests := []struct {
+		File              string
+		VersionName       string
+		Protocol          int32
+		Description       string
+		MaxPlayers        int32
+		OnlinePlayers     int32
+		PlayerSampleCount int
+		PlayerSampleName  string
+		PlayerSampleUuid  string
+		Favicon           string
+	}{
+		{
+			File:              "info.json",
+			VersionName:       "1.13.2",
+			Protocol:          404,
+			Description:       "Hello world",
+			MaxPlayers:        100,
+			OnlinePlayers:     5,
+			PlayerSampleCount: 1,
+			PlayerSampleName:  "Raqbit",
+			PlayerSampleUuid:  "09bc745b-3679-4152-b96b-3f9c59c42059",
+			Favicon:           "data:image/png;base64,<data>",
+		},
+		{
+			File:              "info_description_string.json",
+			VersionName:       "1.13.2",
+			Protocol:          404,
+			Description:       "Hello world",
+			MaxPlayers:        100,
+			OnlinePlayers:     5,
+			PlayerSampleCount: 1,
+			PlayerSampleName:  "Raqbit",
+			PlayerSampleUuid:  "09bc745b-3679-4152-b96b-3f9c59c42059",
+			Favicon:           "data:image/png;base64,<data>",
+		},
+		{
+			File:          "info_description_1_20_3.json",
+			VersionName:   "Paper 1.20.4",
+			Protocol:      765,
+			Description:   "Foo",
+			MaxPlayers:    20,
+			OnlinePlayers: 0,
+		},
 	}
 
-	for _, f := range files {
-		infoJson := GetTestFileContents(t, f)
+	for _, test := range tests {
+		t.Run(test.File, func(t *testing.T) {
+			infoJson := GetTestFileContents(t, test.File)
 
-		info, err := parseServerInfo(infoJson)
+			info, err := parseServerInfo(infoJson)
 
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if info.Version.Name != VersionName {
-			parseError(t, f, "version name")
-		}
-
-		if info.Version.Protocol != Protocol {
-			parseError(t, f, "protocol version")
-		}
-
-		if info.Description.Text != Description {
-			parseError(t, f, "description")
-
-		}
-
-		if info.Players.Max != MaxPlayers {
-			parseError(t, f, "max players")
-
-		}
-
-		if info.Players.Online != OnlinePlayers {
-			parseError(t, f, "online players")
-		}
-
-		if len(info.Players.Sample) != PlayerSampleCount {
-			parseError(t, f, "player sample")
-		} else {
-			if info.Players.Sample[0].Name != PlayerSampleName {
-				parseError(t, f, "player sample name")
+			if err != nil {
+				t.Fatal(err)
 			}
 
-			if info.Players.Sample[0].ID != PlayerSampleUuid {
-				parseError(t, f, "player sample uuid")
+			if info.Version.Name != test.VersionName {
+				parseError(t, test.File, "version name")
 			}
 
-		}
+			if info.Version.Protocol != test.Protocol {
+				parseError(t, test.File, "protocol version")
+			}
 
-		if info.Favicon != Favicon {
-			parseError(t, f, "favicon")
-		}
+			if info.Description.Text != test.Description {
+				parseError(t, test.File, "description")
+
+			}
+
+			if info.Players.Max != test.MaxPlayers {
+				parseError(t, test.File, "max players")
+
+			}
+
+			if info.Players.Online != test.OnlinePlayers {
+				parseError(t, test.File, "online players")
+			}
+
+			if len(info.Players.Sample) != test.PlayerSampleCount {
+				parseError(t, test.File, "player sample")
+			} else if test.PlayerSampleCount > 0 {
+				if info.Players.Sample[0].Name != test.PlayerSampleName {
+					parseError(t, test.File, "player sample name")
+				}
+
+				if info.Players.Sample[0].ID != test.PlayerSampleUuid {
+					parseError(t, test.File, "player sample uuid")
+				}
+
+			}
+
+			if info.Favicon != test.Favicon {
+				parseError(t, test.File, "favicon")
+			}
+
+		})
 	}
 }
 
@@ -82,7 +117,7 @@ func parseError(t *testing.T, file string, name string) {
 func GetTestFileContents(t *testing.T, name string) []byte {
 	path := filepath.Join(TestDataDir, name)
 
-	data, err := ioutil.ReadFile(path)
+	data, err := os.ReadFile(path)
 
 	if err != nil {
 		t.Fatal(err)
